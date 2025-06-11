c3ae78941786b12a3eb2e92279aca076f78574d1
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-11 15:41:40 +0530

    base: Allow user to provide keybox data [1/2]
    Handle PKCS#1 RSA keys for keybox spoofing as well
    
    Change-Id: Ia76f2a06f17e295dffef44312295fab9bd67839f
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -486,5 +486,6 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.EXTRA_DIM_AUTO_MODE, new DiscreteValueValidator(new String[] {"0", "1", "2", "3", "4"}));
         VALIDATORS.put(Secure.EXTRA_DIM_AUTO_TIME, TIME_RANGE_VALIDATOR);
         VALIDATORS.put(Secure.HIDE_APPLIST, ANY_STRING_VALIDATOR);
+        VALIDATORS.put(Secure.KEYBOX_DATA, ANY_STRING_VALIDATOR);
     }
 }
